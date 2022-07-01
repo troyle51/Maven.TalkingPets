1b38c6f57559e99f6b5254f543a35d43add0ed72
Troy <troy@ZipcodersMBP28.fios-router.home>
2022-07-01 18:54:35 -0400

    part 1. Setup for 1 pet

--- src/main/java/io/zipcoder/polymorphism/MainApplication.java
+++ src/main/java/io/zipcoder/polymorphism/MainApplication.java
@@ -1,4 +1,21 @@
 package io.zipcoder.polymorphism;
+import java.util.*;
 
 public class MainApplication {
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hello, how many pets do you have?");
+        int numOfPets = input.nextInt();
+
+        System.out.println("What kind of pet is it?");
+        String petType = input.next();
+
+        System.out.println("What are your pet's names?");
+        String petName = input.next();
+
+        System.out.println("You have " + numOfPets + " pet. " + "Your pet is a " + petType + ". " + "Hello, " + petName + "!");
+    }
+
+
 }
