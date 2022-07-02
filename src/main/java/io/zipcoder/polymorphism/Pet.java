90e7857a445b206c211f2f21a77a55b9ccfbbc08
Troy <troy@Zipcoders-MacBook-Pro-28.local>
2022-07-02 08:31:55 -0400

    class pet created

---
+++ src/main/java/io/zipcoder/polymorphism/Pet.java
@@ -0,0 +1,31 @@
+package io.zipcoder.polymorphism;
+
+public class Pet {
+    private String name;
+    private String breed;
+    private int age;
+    private int id;
+
+    public Pet(String name, String breed,) {
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+        this.id = id;
+    }
+
+    public String getName() { return name; }
+
+    public void setName(String name) { return name; }
+
+    public String getBreed() { return breed; }
+
+    public void setBreed(String breed) { return breed; }
+
+    public Integer getAge() { return age; }
+    public Integer getId() { return id; }
+
+
+
+}
+
+
